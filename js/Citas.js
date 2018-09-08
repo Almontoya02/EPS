62a7fa4907d7441291cc99ca23e5c85f2015f3d2
AzothGuyx <cristianxapata@gmail.com>
2018-09-08 18:44:03 -0500

    js de citas

---
+++ js/Citas.js
@@ -0,0 +1,107 @@
+$(function () {
+    var operation = "C";
+    var selected_index = -1;
+    var tblCitas = localStorage.getItem("tblCitas"); 
+    tblCitas = JSON.parse(tblCitas); 
+    if (tblCitas === null)
+        tblCitas = [];
+  
+    function Create() {
+    
+      var citas = JSON.stringify({
+        Cedula: $("#cedulaPaciente").val(),
+        Fecha: $("#fechaCita").val(),
+        Hora: $("#horaCita").val(),
+        Doctor: $("#selectDoctor").val()
+      }); 
+    
+      tblCitas.push(citas);
+   
+      localStorage.setItem("tblCitas", JSON.stringify(tblCitas));
+      alert("La cita ha sido guardada");
+      return true;
+    }
+  
+    function Edit() {
+  
+      tblCitas[selected_index] = JSON.stringify({
+        Cedula: $("#cedulaPaciente").val(),
+        Fecha: $("#fechaCita").val(),
+        Hora: $("#horaCita").val(),
+        Doctor: $("#selectDoctor").val()
+      });
+  
+      localStorage.setItem("tblCitas", JSON.stringify(tblCitas));
+      alert("La cita ha sido Editada");
+      return true;
+    }
+  
+    function Delete() {
+     
+      tblCitas.splice(selected_index, 1); 
+     
+      localStorage.setItem("tblCitas", JSON.stringify(tblCitas));
+      alert("La cita ha sido borrada"); 
+    }
+  
+    function List() {
+      $("#tblList").html("");
+      $("#tblList").html(
+              "<thead>" +
+              "<tr>" +                
+              "<th>Cedula</th>" +
+              "<th>Fecha</th>" +
+              "<th>Hora</th>" +
+              "<th>Doctor</th>" +
+              "<th>Action</th>" +
+              "</tr>" +
+              "</thead>" +
+              "<tbody>" +
+              "</tbody>"
+              ); 
+      for (var i in tblCitas) {
+          var cita = JSON.parse(tblCitas[i]);
+          $("#tblList tbody").append("<tr>" +                    
+                  "<td>" + cita.Cedula + "</td>" +
+                  "<td>" + cita.Fecha + "</td>" +
+                  "<td>" + cita.Hora+ "</td>" +
+                  "<td>" + cita.Doctor + "</td>" +                    
+                  "</tr>"
+                  );
+      } 
+    }
+  
+    $("#formCitas").bind("submit", function () {
+      if (operation === "C")
+          return Create();
+      else
+          return Edit();
+    }); 
+    
+    List();
+    
+    /**
+     * no he hecho esto :v (lo de abajo)
+     */
+  
+    $(".btnEdit").bind("click", function () {
+      operation = "E"; 
+      
+      selected_index = parseInt($(this).attr("alt").replace("Edit", ""));
+      
+      var per = JSON.parse(tblPersons[selected_index]); 
+      $("#txtYears").val(per.Years);
+      $("#txtMotivation").val(per.Motivation);
+      $("#txtGoals").val(per.Goals);
+      $("#txtReason").val(per.Reason);
+      $("#txtYears").attr("readonly", "readonly");
+      $("#txtMotivation").focus();
+    });
+  
+    $(".btnDelete").bind("click", function () {
+  
+      selected_index = parseInt($(this).attr("alt").replace("Delete", "")); 
+      Delete(); 
+      List(); 
+    });
+  });
