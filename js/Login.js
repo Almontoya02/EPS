8ee548ce79f32010d8434a3ae527185341773d3c
AzothGuyx <cristianxapata@gmail.com>
2018-09-08 20:01:10 -0500

    logica del login

---
+++ js/Login.js
@@ -0,0 +1,66 @@
+var login=function(){
+    
+    /**
+     * arreglo de doctores
+     */
+    var doctores=[
+        {
+            user:1234,
+            password:'1234'
+        },
+        {
+            user:6521,
+            password:'abcd'
+        },
+        {
+            user:6451,
+            password:'code'
+        }
+
+    ];
+
+    /**
+     * arreglo de secretarias
+     */
+    var secretarias=[
+        {
+            user:789,
+            password:'1234'
+        },
+        {
+            user:007,
+            password:'abcd'
+        },
+        {
+            user:1011,
+            password:'code'
+        }
+
+    ];
+
+    /**
+     * arreglo de admins
+     * 
+     */
+    var admins=[
+        {
+            user:0000,
+            password:'1234'
+        },
+        {
+            user:543,
+            password:'abcd'
+        },
+        {
+            user:210,
+            password:'code'
+        }
+
+    ];
+
+
+
+
+
+
+}
